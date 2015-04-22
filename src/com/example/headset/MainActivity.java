0181be714e200e33f276f6660a2eb01b7c4c2452
Thundersoft_zhaocg0422 <zhaocg0422@thundersoft.com>
2015-04-22 11:57:57 +0800

    add the function of headset detection

--- src/com/example/headset/MainActivity.java
+++ src/com/example/headset/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.headset;
 
 import android.app.Activity;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+	private HeadsetPlugReceiver headsetPlugReceiver; 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		/* register receiver */  
+	    registerHeadsetPlugReceiver(); 
 	}
+	
+	private void registerHeadsetPlugReceiver() {  
+	    headsetPlugReceiver = new HeadsetPlugReceiver();
+	    IntentFilter intentFilter = new IntentFilter();  
+	    intentFilter.addAction("android.intent.action.HEADSET_PLUG");  
+	    registerReceiver(headsetPlugReceiver, intentFilter);  
+	  }
+	
+	 @Override  
+	  public void onDestroy() {  
+	    unregisterReceiver(headsetPlugReceiver);  
+	    super.onDestroy();  
+	  }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
